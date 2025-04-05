8fcfae69b77e3b0bb4179f3f67885aa29db9051b
jamalihassan0307 <jamalihassan0307@gmail.com>
2025-04-05 07:01:56 +0500

    Export additional modules for performance monitoring and tracking in flutter_pulse.dart

--- lib/flutter_pulse.dart
+++ lib/flutter_pulse.dart
@@ -1,5 +1,17 @@
 library flutter_pulse;
 
+export 'src/performance_monitor.dart';
+export 'src/config/monitor_config.dart';
+export 'src/widgets/performance_dashboard.dart';
+export 'src/theme/dashboard_theme.dart';
+export 'src/models/log_level.dart';
+export 'src/trackers/fps_tracker.dart';
+export 'src/trackers/memory_tracker.dart';
+export 'src/trackers/cpu_tracker.dart';
+export 'src/trackers/network_tracker.dart';
+export 'src/trackers/battery_tracker.dart';
+export 'src/trackers/device_info_tracker.dart';
+
 /// A Calculator.
 class Calculator {
   /// Returns [value] plus 1.
