a0871a15fbab6f6e7cd71a0f398c45ccf3db7f8e
jamalihassan0307 <jamalihassan0307@gmail.com>
2025-04-05 07:26:33 +0500

    Update import statement in widget_test.dart to use flutter_performance_pulse_example for consistency with recent project naming changes.

--- example/test/widget_test.dart
+++ example/test/widget_test.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:flutter_pulse_example/main.dart';
+import 'package:flutter_performance_pulse_example/main.dart';
 
 void main() {
   testWidgets('Example app smoke test', (WidgetTester tester) async {
